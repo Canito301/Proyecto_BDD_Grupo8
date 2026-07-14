e103c234e9ab8de32555875016dbf62f5b848ca8
JP3dr3 <jpedreros2024@udec.cl>
2026-07-14 00:06:04 -0400

    Arreglo conexion base de datos.

--- empresa_buses/settings.py
+++ empresa_buses/settings.py
@@ -53,13 +53,13 @@ WSGI_APPLICATION = 'empresa_buses.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'proyectobdd8def',
-        'USER': 'grupo8',
-        'PASSWORD': 'ProyectoBDD_8',
+        'NAME': 'empresa_buses',
+        'USER': 'postgres',
+        'PASSWORD': 'pass1234',
         'HOST': 'localhost',
         'PORT': '5432',
         'OPTIONS': {
-            'options': '-c search_path=empresa_buses,terminal_buses_sj,public'
+            'options': '-c search_path=empresa_buses,public'
         },
     }
 }
